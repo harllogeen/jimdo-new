5efaac2a54985640a8b4104518af5588069bda7b
Alugbin Oluwaseyi John <harllogeen@harllogeen2020@gmail.com>
2023-04-26 13:19:30 +0100

    ok ok

--- src/Views/Auth/Login.js
+++ src/Views/Auth/Login.js
@@ -36,7 +36,7 @@ const Login = () => {
   // })
   // .then(response => response.json())
   // .then(data => {
-      //store JWT token in local storage or cookie
+  //store JWT token in local storage or cookie
   //     localStorage.setItem('token', data.token)
   // })
   // .catch(error => console.error(error));
@@ -48,7 +48,7 @@ const Login = () => {
   // })
   // .then(response => response.json())
   // .then(data => {
-    //Handle response data
+  //Handle response data
   // })
   // .catch(error => console.error(error))
 
@@ -110,6 +110,7 @@ const Login = () => {
                     name="password"
                     type={open === false ? "password" : "text"}
                     placeholder="Password"
+                    
                   />
                   <GrLock className="absolute top-4 left-3 text-xl" />
                   <div className="text-2xl absolute top-4 right-5">
--- src/Views/Auth/TextField.js
+++ src/Views/Auth/TextField.js
@@ -3,11 +3,13 @@ import { ErrorMessage, useField } from "formik";
 
 export const TextField = ({ label, ...props }) => {
   const [field, meta] = useField(props);
+  
   return (
+    
     <div className="mb-2 items-center justify-center">
       <input
-        className={`form-control  w-full  h-12 opacity-100 bg-[#F7F7F7]  pl-14 text-sm mb-1 items-center justify-center  outline-none shadow-none ${
-          meta.touched && meta.error && "is-invalid"
+        className={`form-control border-2  w-full  h-12 opacity-100 bg-[#F7F7F7]  pl-14 text-sm mb-1 items-center justify-center  outline-none shadow-none ${
+          meta.touched && meta.error ? 'border-fcred' : 'border-primary'
         }`}
         {...field}
         {...props}
